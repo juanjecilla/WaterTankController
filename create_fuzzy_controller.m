e8fd4973fb271c1c8e95e3c2d10c0b3c59715493
juanjecilla <juanje.cilla@gmail.com>
2018-06-27 12:02:46 +0200

    Rules updated

--- create_fuzzy_controller.m
+++ create_fuzzy_controller.m
@@ -13,7 +13,40 @@ FIS = addmf(FIS,'input',2,'Medium','trapmf',[7.5 10 12.5 15]);
 FIS = addmf(FIS,'input',2,'High','trimf',[12.5 15 17.5 20]);
 FIS = addmf(FIS,'input',2,'Too High','trimf',[17.5 20 25]);
 
-FIS = addvar(FIS,'output','Water level current',[5 20]);
-FIS = addmf(FIS,'output',1,'Low','trimf',[5 7.5 10]);
-FIS = addmf(FIS,'output',1,'Medium','trapmf',[7.5 10 12.5 15]);
-FIS = addmf(FIS,'output',1,'High','trimf',[12.5 15 17.5 20]);
+FIS = addvar(FIS,'output','Water level current',[0 5]);
+FIS = addmf(FIS,'output',1,'Low','trimf',[-1 0 1]);
+FIS = addmf(FIS,'output',1,'Medium','trapmf',[0 1 3 4]);
+FIS = addmf(FIS,'output',1,'High','trimf',[4 5 6]);
+
+{[1 1];
+ [1 2];
+ [1 3];
+ [1 4];
+ [1 5];
+ [2 5];
+ [2 4];
+ [2 3];
+ [2 2];
+ [2 1];
+ [3 1];
+ [3 2];
+ [3 3];
+ [3 4];
+ [3 5]}
+[3;3;2;2;2;1;1;2;2;2;3;2;2;2;1]
+ruleList = [1 1 3 1 1;   % Rule 1
+            1 2 3 1 1;   % Rule 2
+            1 3 2 1 1;   % Rule 3
+            1 4 2 1 1;   % Rule 4
+            1 5 2 1 1;   % Rule 5
+            2 5 1 1 1;   % Rule 6
+            2 4 1 1 1;   % Rule 7
+            2 3 2 1 1;   % Rule 8
+            2 2 2 1 1;   % Rule 9
+            2 1 2 1 1;   % Rule 10
+            3 1 3 1 1;   % Rule 11
+            3 2 2 1 1;   % Rule 12
+            3 3 2 1 1;   % Rule 13
+            3 4 2 1 1;   % Rule 14
+            3 5 1 1 1];  % Rule 15
+FIS = addrule(FIS, ruleList);
